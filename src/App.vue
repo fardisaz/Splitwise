b4e1a3b0c7ba130b067ca97c33a620b83ae30cd1
araei <fardis.araei@beg.de>
2026-03-27 15:28:50 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <div class="wrapper">
+    <h1>You did it!</h1>
+    <input type="text" />
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  font-family: 'Lucida Console', 'Courier New', monospace;
+  text-align: center;
+}
+input{
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <input/>
+    <v-for="(item, index) in items" :key="index"
+  </div>
+</template>
+<style scoped>
+input{
+margin:20px;
+}
+</style>
